9290c0374b006d6441506f062c5a422516589183
Rinachi <info@vmatthias.hu>
2025-05-11 08:58:27 +0200

    created entities

---
+++ src/main/java/PcGameCollectionApp/maindata/Developer.java
@@ -0,0 +1,26 @@
+package PcGameCollectionApp.maindata;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Developer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String name;
+
+}
