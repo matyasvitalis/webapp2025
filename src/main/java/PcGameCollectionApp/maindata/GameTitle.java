cfca0a01fb3d7dea5c4adc44146030870c542d20
Rinachi <info@vmatthias.hu>
2025-05-11 08:58:27 +0200

    created entities

---
+++ src/main/java/PcGameCollectionApp/maindata/GameTitle.java
@@ -0,0 +1,27 @@
+package PcGameCollectionApp.maindata;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class GameTitle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String gameTitle;
+
+}
